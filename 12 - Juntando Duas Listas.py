25c23fcf63ce5d90d91b979d92aa3ccb467c13a2
KevinAFS <kevinafsilva10@gmail.com>
2025-08-12 22:44:51 -0300

    solução concluída do {Juntando Duas Listas}

---
+++ 12 - Juntando Duas Listas.py
@@ -0,0 +1,8 @@
+def addlist (ol, sl):
+    return ol + sl
+
+natureza = ["rio", "floresta", "leão", "montanha", "oceano", "estrela", "vento", "cachoeira", "lua", "girassol"]
+tecnologia = ["algoritmo", "API", "blockchain", "código", "software", "nuvem", "hardware", "interface", "pixel", "servidor"]
+
+result = addlist(natureza, tecnologia)
+print(result)
