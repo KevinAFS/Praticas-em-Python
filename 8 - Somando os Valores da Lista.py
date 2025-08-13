89d590d13e1c6f24c563dff5d4e85c0d4cd08682
KevinAFS <kevinafsilva10@gmail.com>
2025-08-12 22:41:10 -0300

    solução conclída do {Extraindo Nomes de Objetos}

---
+++ 8 - Somando os Valores da Lista.py
@@ -0,0 +1,10 @@
+def add(numbers):
+    current = 0
+    for i in numbers:
+        current = current + i
+    return current 
+numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+
+currnum = add(numeros)
+print (currnum)
+
