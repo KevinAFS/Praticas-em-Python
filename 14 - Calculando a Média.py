8912c3e7ff84f5745459239533b14213742e367e
KevinAFS <kevinafsilva10@gmail.com>
2025-08-12 22:45:59 -0300

    solução concluída do {Calculando a Média}

---
+++ 14 - Calculando a Média.py
@@ -0,0 +1,10 @@
+def media (numbers):
+    if not numbers:
+        return 0
+    med = (sum(numbers)) / len(numbers)
+    return med
+
+numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+
+medi = media (numeros)
+print(medi)
